8771ec34129bfdd527bebd9405edd9589084b980
mrimosthe1 <msarkar07@live.in>
2017-03-23 16:39:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,10 @@
 console.log('Loaded!');
 var element=document.getElementById('main-element');
 element.innerHTML='complete two weeks today';
+
+//move image
+var img=document.getElementById('madi');
+
+img.onclick=function(){
+  img.style.marginleft="100px";  
+};
